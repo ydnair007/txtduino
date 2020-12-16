4cd7e37e7f2a8f2efd90d4eb72e3427aae9554ea
Yadukrishnan Nair <33169773+ydnair007@users.noreply.github.com>
2020-12-17 02:02:24 +0530

    Update txtduino.py

--- txtduino.py
+++ txtduino.py
@@ -9,7 +9,7 @@ try:
     client = TwilioRestClient(settings.twilio_account_sid,
                           settings.twilio_auth_token)
     message = client.sms.messages.create(
-        body="File Downloaded!",
+        body="Master File Downloaded and uploaded to Gdrive!",
         to=settings.your_phone_number,
         from_=settings.your_twilio_number)
 
